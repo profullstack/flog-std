4baa555ea658bc7569a4abe342b2b66c6cea7077
terrablue <102580937+terrablue@users.noreply.github.com>
2023-01-07 00:18:59 +0200

    dyndef: update spec files to reflect new debris version

--- dyndef/src/Is.spec.js
+++ dyndef/src/Is.spec.js
@@ -1,9 +1,6 @@
-import {Test} from "debris";
 import Is from "Is.js";
 
-const test = new Test();
-
-const fixtures = {
+const fixtures = Object.entries({
   string: ["", String()],
   number: [0, Number(0)],
   bigint: [0n],
@@ -12,20 +9,18 @@ const fixtures = {
   function: [() => undefined, function() { return undefined; }],
   null: [null],
   undefined: [undefined],
-};
-
-const entries = Object.entries(fixtures);
-
-test.case("non objects", assert => {
-  entries.forEach(([key, values]) => {
-    const non_values = entries.filter(entry => entry[0] !== key)
-      .flatMap(([, value]) => value);
-    values.forEach(value => {
-      assert(new Is(value)[key]()).equals(value);
-      non_values.forEach(non_value =>
-        assert(() => new Is(non_value)[key]()).throws())
-    });
-  });
 });
 
-export default test;
+export default test => {
+  test.case("non objects", assert => {
+    fixtures.forEach(([key, values]) => {
+      const nonValues = fixtures.filter(entry => entry[0] !== key)
+        .flatMap(([, value]) => value);
+      values.forEach(value => {
+        assert(new Is(value)[key]()).equals(value);
+        non_values.forEach(nonValue =>
+          assert(() => new Is(nonValue)[key]()).throws())
+      });
+    });
+  });
+};
--- dyndef/src/maybe.spec.js
+++ dyndef/src/maybe.spec.js
@@ -1,16 +1,13 @@
-import {Test} from "debris";
 import maybe from "maybe.js";
 
-const test = new Test();
+export default test => {
+  test.case("nullish", assert => {
+    assert(maybe(undefined).number()).undefined();
+    assert(maybe(null).string()).equals(null);
+  });
 
-test.case("nullish", assert => {
-  assert(maybe(undefined).n()).undefined();
-  assert(maybe(null).s()).equals(null);
-});
-
-test.case("non-nullish", assert => {
-  assert(maybe(0).n()).equals(0);
-  assert(maybe("0").s()).equals("0");
-});
-
-export default test;
+  test.case("non-nullish", assert => {
+    assert(maybe(0).number()).equals(0);
+    assert(maybe("0").string()).equals("0");
+  });
+};
