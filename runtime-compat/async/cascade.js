c2f3c83082a16ea81376288367caf77c9fcce7f1
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-06 17:43:44 +0300

    runtime-compat/async: add cascade

---
+++ runtime-compat/async/cascade.js
@@ -0,0 +1,26 @@
+import {identity} from "../function/exports.js";
+import {is} from "../dyndef/exports.js";
+
+/*
+ * given a list of functions that accept `(input, next)` with the general
+ * expectation they call `next(maybe_transformed_input)`, resolve them
+ * returning the transformed input.
+ *
+ * This is essentially a form of currying, where given
+ * [fn0, fn1, ... fn[N]], this structure is produced:
+ * fn0(input, fn0_out => fn1(fn0_out, fn1_out => ... fn[N](fn[N-1]_out)))
+ *
+ * fn[N] is expected *not* to call `next`, and is if omitted a noop (identity)
+ *
+ * Midchain breakages can be deemed acceptable in some scenarios, where a
+ * function may elect to determine the output is done and not call `next`
+ *
+ * reduceRight here essentially wraps every function, starting from the last,
+ * in the next last, until the first function is the outermost wrapper,
+ * executed first
+ */
+export default (fns, initial_fn = identity) => {
+  is(fns).array();
+
+  return fns.reduceRight((next, fn) => value => fn(value, next), initial_fn);
+};
--- runtime-compat/async/exports.js
+++ runtime-compat/async/exports.js
@@ -1,1 +1,2 @@
+export {default as cascade} from "./cascade.js";
 export {default as tryreturn} from "./tryreturn.js";
