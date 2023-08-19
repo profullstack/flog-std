65be351d3dd45718705afcc5e6f4e7dd173d4efb
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-19 16:49:35 +0300

    dyndef: improve debugging

--- runtime-compat/dyndef/src/Is.js
+++ runtime-compat/dyndef/src/Is.js
@@ -114,8 +114,8 @@ export default class Is {
 
   anyOf(Classes, error) {
     const classes = Classes instanceof Array ? Classes : [Classes];
-    const classes_str = classes.map(c => `\`${c}\``).join(", ");
-    const def = `\`${this.#value}\` must instance any of \`${classes_str}\``;
+    const classes_str = classes.map(c => `\`${c?.name ?? name}\``).join(", ");
+    const def = `\`${this.#value}\` must instance any of ${classes_str}`;
     const condition = classes.some(c => try_instanceof(this.#value, c));
     return this.#test({condition, def, error});
   }
