abba1a5204143c493ec73fa38ad17ec9a8e934dd
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-24 17:26:49 +0300

    add object.stringify

--- runtime-compat/object/exports.js
+++ runtime-compat/object/exports.js
@@ -7,3 +7,4 @@ export {default as map} from "./src/map.js";
 export {default as keymap} from "./src/keymap.js";
 export {default as valmap} from "./src/valmap.js";
 export {default as filter} from "./src/filter.js";
+export {default as stringify} from "./src/stringify.js";
